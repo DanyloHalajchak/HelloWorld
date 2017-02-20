cb5565a6b39fff074cc4e558728691f5d0ff0816
user <user@MICROSOFT-PC>
2017-02-20 23:33:35 +0200

    67565e6u

--- HelloWord/src/ua/less1/Main.java
+++ HelloWord/src/ua/less1/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		System.out.println("Hellow World");
 		System.out.println("Hellow World");
 		System.out.println("Hellow World");
+		System.out.println("Hellow World");
 	}
 
 }
